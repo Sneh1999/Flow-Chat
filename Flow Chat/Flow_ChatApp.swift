8645f83e37abb2946468312e611932b7b1a714bc
Sneh1999 <snehkoul1999@gmail.com>
2023-02-19 10:35:41 -0600

    Initial Commit

---
+++ Flow Chat/Flow_ChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  Flow_ChatApp.swift
+//  Flow Chat
+//
+//  Created by Sneh Koul on 2023-02-19.
+//
+
+import SwiftUI
+
+@main
+struct Flow_ChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
